8756258334d405966f840abf740f1f8813a5ff66
Timothy-Barry <timothybarry@cmu.edu>
2021-01-01 20:30:08 -0500

    remove get tests

--- tests/testthat/test_gets.R
+++ tests/testthat/test_gets.R
@@ -1,3 +1,5 @@
+if (FALSE) {
+
 test_that("gets, no subsets", {
   for (i in 1:n_datasets) {
     if (n_datasets > 1) cat(paste0("Running test ", i, ".\n"))
@@ -40,3 +42,4 @@ test_that("gets after subset", {
       }
     }
 })
+}
