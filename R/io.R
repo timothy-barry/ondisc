0c2bc77684128f72e8d84c7e1f51ccac2bb70964
Tim <tbarry2@andrew.cmu.edu>
2021-08-04 21:30:29 -0400

    improve save/read odm functions; allow user to specify a name by which to identify variants of a given expression matrix.

--- R/io.R
+++ R/io.R
@@ -3,6 +3,7 @@
 #' Saves an ondisc matrix or metadata ondisc matrix.
 #'
 #' @param odm a metadata_ondisc_matrix object or ondisc_matrix object
+#' @param name name to label save
 #'
 #' @return NULL
 #' @export
@@ -18,9 +19,11 @@
 #' features_fp = file_locs[["features"]],
 #' odm_fp = odm_fp,
 #' return_metadata_ondisc_matrix = TRUE)
-#' save_odm(expression_data)
-#' expression_data_loaded <- read_odm(odm_fp)
-save_odm <- function(odm) {
+#' expression_data <- expression_data[,1:10]
+#' save_odm(expression_data, "filtered")
+#' expression_data_loaded_unfiltered <- read_odm(odm_fp)
+#' expression_data_loaded_filtered <- read_odm(odm_fp, "filtered")
+save_odm <- function(odm, name) {
   aux_info <- list()
   if (is(odm, "multimodal_ondisc_matrix")) {
     stop("Save function not yet implemented for metadata_ondisc_matrix class.
@@ -35,7 +38,11 @@ save_odm <- function(odm) {
   aux_info$cell_subset <- odm@cell_subset
   aux_info$feature_subset <- odm@feature_subset
 
-  aux_info_fp <- paste0(odm@odm_fp, "/aux_info.rds")
+  # determine file path to save
+  append_rds <- !grepl(pattern = "*.rds$", x = name)
+  aux_info_fp <- paste0(odm@odm_fp, "/", name, if (append_rds) ".rds" else "")
+
+  # save
   saveRDS(object = aux_info, file = aux_info_fp)
 }
 
@@ -44,21 +51,24 @@ save_odm <- function(odm) {
 #' @rdname save_odm
 #' @export
 #' @param odm_fp file path to an ondisc matrix (or metadata ondisc matrix)
-read_odm <- function(odm_fp) {
+read_odm <- function(odm_fp, name = NULL) {
   h5_file <- paste0(odm_fp, "/data.h5")
-  aux_info_file <- paste0(odm_fp, "/aux_info.rds")
-  underlying_dimension <- as.integer(rhdf5::h5read(file = h5_file, name = "dimension"))
-  logical_mat <- as.logical(rhdf5::h5read(file = h5_file, name = "logical_mat"))
-  out <- internal_initialize_ondisc_matrix(odm_fp = odm_fp, logical_mat = logical_mat, underlying_dimension = underlying_dimension)
-  # read the aux info .rds file
-  aux_info <- readRDS(aux_info_file)
-  out@cell_subset <- aux_info$cell_subset
-  out@feature_subset <- aux_info$feature_subset
-  # check if feature covariates and cell covariates are present; if so, coerce to metadata_odm
-  if ("cell_covariates" %in% names(aux_info) && "feature_covariates" %in% names(aux_info)) {
-    out <- metadata_ondisc_matrix(ondisc_matrix = out,
-                           cell_covariates = aux_info$cell_covariates,
-                           feature_covariates = aux_info$feature_covariates)
+  # construct the ondisc_matrix object
+  out <- ondisc_matrix(h5_file)
+  # if name is non-null, modify the output futher
+  if (!is.null(name)) {
+    append_rds <- !grepl(pattern = "*.rds$", x = name)
+    aux_info_fp <- paste0(odm_fp, "/", name, if (append_rds) ".rds" else "")
+    # load aux info rds; update cell and feature idxs
+    aux_info <- readRDS(aux_info_fp)
+    out@cell_subset <- aux_info$cell_subset
+    out@feature_subset <- aux_info$feature_subset
+    # if available, load cell-specific and feature-specific covariates; return metadata_odm
+    if ("cell_covariates" %in% names(aux_info) && "feature_covariates" %in% names(aux_info)) {
+      out <- metadata_ondisc_matrix(ondisc_matrix = out,
+                                    cell_covariates = aux_info$cell_covariates,
+                                    feature_covariates = aux_info$feature_covariates)
+    }
   }
   return(out)
 }
