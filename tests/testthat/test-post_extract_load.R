a46e87e8c22f38d9c3f7dc36180fdaa55ec0d17c
Tim <tbarry2@andrew.cmu.edu>
2022-04-04 18:06:32 -0400

    finish test for normalize by regression

--- tests/testthat/test-post_extract_load.R
+++ tests/testthat/test-post_extract_load.R
@@ -16,18 +16,23 @@ test_that("normalize by regression", {
     # normalize by regression
     cov_odm <- cov_odm[my_features, good_cells]
     cov_odm <- cov_odm %>% mutate_cell_covariates(lg_n_umis = log(n_umis), lg_n_nonzero = log(n_nonzero))
-    cov_odm_normed <- normalize_by_regression(covariate_odm = cov_odm)
 
-    # run regression on the selected features and manually extract Pearson resids
-    my_form <- paste0("feature_exp ~ ", paste0(cov_odm_normed@misc$covariates, collapse = " + "), " + offset(", cov_odm_normed@misc$offset, ")")
+    # choose covariates on which to regress
+    possible_covariates <- c("p_mito", "lg_n_nonzero")
+    covariates_to_regress_on <- sample(x = possible_covariates, size = sample(x = seq(0, length(possible_covariates)), size = 1))
+    if (length(covariates_to_regress_on) == 0) covariates_to_regress_on <- NULL
+    my_form <- paste0("feature_exp ~ ", paste0(covariates_to_regress_on, collapse = " + "), " + offset(lg_n_umis)")
+
+    # perform the regression
     covariate_matrix <- get_cell_covariates(cov_odm)
-
     r_resids <- sapply(X = my_features, FUN = function(my_feat) {
       feature_exp <- as.numeric(cov_odm[[my_feat,]])
       fit <- glm(formula = my_form, family = stats::poisson(),
                  data = dplyr::mutate(covariate_matrix, feature_exp))
       pearson_resid <- resid(fit, "pearson")
     }) %>% t()
+    # normalize using ondisc function
+    cov_odm_normed <- normalize_by_regression(covariate_odm = cov_odm, covariates = covariates_to_regress_on)
     expect_true(all((r_resids - cov_odm_normed[[my_features,]]) < 1e-4))
     # random subset by cells
     my_cell_subset <- sample(x = seq(1, ncol(cov_odm_normed)), size = sample(x = seq(1, ncol(cov_odm_normed)), size = 1), replace = FALSE)
