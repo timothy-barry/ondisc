510f526d4322bdd7b8aa5e49a8ba4aaebc1d131e
Tim <tbarry2@andrew.cmu.edu>
2022-08-17 11:39:15 -0400

    update thin_multimodal_odm function; save data frame with n_cells number of rows in cell covariates

--- R/crispr_screen.R
+++ R/crispr_screen.R
@@ -108,12 +108,14 @@ load_thresholded_and_grouped_grna <- function(covariate_odm, grna_group, thresho
 #' @param gene_modality_name the name of the gene modality
 #'
 #' @return a "thinned" multimodal ondisc matrix
-thin_multimodal_odm <- function(multimodal_odm, grna_modality_name, gene_modality_name) {
+thin_multimodal_odm <- function(multimodal_odm, grna_modality_name, gene_modality_name, grna_group_column_name) {
   row.names(multimodal_odm@global_cell_covariates) <- NULL
 
+  multimodal_odm@modalities <- multimodal_odm@modalities[c(grna_modality_name, gene_modality_name)]
   # clear out (i) cell covariates df, (ii) misc list, (iii) feature names, (iv) cell barcodes
   for (modality in c(grna_modality_name, gene_modality_name)) {
-    multimodal_odm@modalities[[modality]]@cell_covariates <- data.frame()
+    modality_dim <- dim(multimodal_odm@modalities[[modality]])
+    multimodal_odm@modalities[[modality]]@cell_covariates <- as.data.frame(matrix(nrow = modality_dim[2], ncol = 0))
     multimodal_odm@modalities[[modality]]@misc <- list()
     multimodal_odm@modalities[[modality]]@ondisc_matrix@feature_names <- ""
     multimodal_odm@modalities[[modality]]@ondisc_matrix@cell_barcodes <- ""
@@ -121,9 +123,9 @@ thin_multimodal_odm <- function(multimodal_odm, grna_modality_name, gene_modalit
 
   # clear out feature df of gene modality
   multimodal_odm@modalities[[gene_modality_name]]@feature_covariates <- data.frame()
-  # clear out columns of feature df of grna modality that are not "target" or "grna_group"
+  # clear out columns of feature df of grna modality that are not "grna_group"
   multimodal_odm@modalities[[grna_modality_name]]@feature_covariates <-
-    multimodal_odm@modalities[[grna_modality_name]]@feature_covariates |> dplyr::select(target, grna_group)
+    multimodal_odm@modalities[[grna_modality_name]]@feature_covariates |> dplyr::select(grna_group_column_name)
 
   return(multimodal_odm)
 }
