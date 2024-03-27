43cc8658986a97df9769369d79545f9f634876ca
timothy-barry <tbarry2@andrew.cmu.edu>
2024-03-27 19:38:55 -0400

    update import data from r matrices

--- R/import_data_from_r_matrices.R
+++ R/import_data_from_r_matrices.R
@@ -1,31 +1,45 @@
 #' Create ODM from R matrix
 #'
-#' @param mat an R matrix of class "dgRMatrix"
-#' @param file_to_write name of the file in which to write the backing `.odm` file
+#' `create_odm_from_r_matrix()` creates an `odm` object from an input R matrix.
+#'
+#' @note
+#' Users do not need to specify the `integer_id` argument; this argument is used by functions within the `sceptre` package that call `create_odm_from_r_matrix()`.
+#'
+#' @param mat an R matrix of class "matrix", "dgCMatrix", "dgRMatrix", or "dgTMatrix"
+#' @param file_to_write file path specifying the location in which to write the backing `.odm` file
 #' @param chunk_size chunk size to use in the backing HDF5 file
 #' @param compression_level compression level to use in the backing HDF5 file
-#' @param integer_id ID used to identify the `.odm` file
+#' @param integer_id integer ID to write to the backing `.odm` file; users need not specify this argument.
 #'
 #' @return an odm object representing the data
-#' @noRd
 #'
 #' @examples
-#' \dontrun{
 #' library(Matrix)
 #' set.seed(4)
 #' n_row <- 500
 #' n_col <- 800
 #' v <- rpois(n = n_row * n_col, lambda = 1)
 #' v[rbinom(n = n_row * n_col, size = 1, prob = 0.8) == 1] <- 0L
-#' m <- matrix(data = v, nrow = n_row, ncol = n_col)
-#' rownames(m) <- paste0("gene_", seq(1L, n_row))
-#' mat <- as(m, "RsparseMatrix")
-#' odm <- create_odm_from_r_matrix(mat, paste0(tempdir(), "gene.odm"))
-#' }
+#' mat <- matrix(data = v, nrow = n_row, ncol = n_col)
+#' rownames(mat) <- paste0("gene_", seq_len(nrow(mat)))
+#' file_to_write <- paste0(tempdir(), "/gene.odm")
+#' odm <- create_odm_from_r_matrix(mat, file_to_write)
 create_odm_from_r_matrix <- function(mat, file_to_write, chunk_size = 1000L, compression_level = 3L, integer_id = 0L) {
-  # 0. check that the class is correct
-  if (!methods::is(mat, "dgRMatrix")) stop("`mat` must be an object of class `dgRMatrix`.")
+  # convert the matrix into a dgRMatrix using the sceptre function
+  mat <- sceptre:::set_matrix_accessibility(mat)
+
+  # expand tilde for the file to write
   file_to_write <- expand_tilde(file_to_write)
+
+  # check for rownames
+  if (is.null(rownames(mat))) {
+    stop("Row names must be supplied for `mat`.")
+  }
+
+  # verify that chunk_size does not exceed data size
+  if (length(mat@j) <= chunk_size) {
+    stop("Decrease the chunk size.")
+  }
 
   # initialize odm
   create_odm_r_matrix_cpp(file_name_in = file_to_write,
