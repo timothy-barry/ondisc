01e7723415dbc566d4bd4373eaad4d1bac49c4e8
timothy-barry <tbarry2@andrew.cmu.edu>
2024-03-31 13:46:40 -0400

    add vignette

--- vignettes/update_vignette.sh
+++ vignettes/update_vignette.sh
@@ -1,5 +1,6 @@
 cp ~/research_code/sceptre-book/ondisc.qmd ~/research_code/ondisc/vignettes/ondisc.Rmd
 
+sed -i -e 's/# The `ondisc` package//g' ondisc.Rmd
 sed -i -e '/::: callout-note/,/:::/d' ondisc.Rmd
 sed -i -e 's|\[frequently asked questions page\](faq.qmd)|\[frequently asked questions page\](https://timothy-barry.github.io/sceptre-book/faq.html)|g' ondisc.Rmd
 sed -i -e 's/{#[^}]*}//g' ondisc.Rmd
